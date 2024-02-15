f16d9ce8ac98f19081e18b96d6541996d91a7506
unstoppable840 <unstoppable840@gmail.com>
2024-02-15 21:45:08 +0800

    bug: 修复了消息操作台的显示bug

--- src/components/chatSpace/chatSpace.tsx
+++ src/components/chatSpace/chatSpace.tsx
@@ -43,59 +43,62 @@ const ChatSpace = (props: any) => {
       e.target.parentElement.parentElement.parentElement.querySelector(
         '#opera'
       );
-    const nextSbling = e.target.nextElementSibling;
-    const { height: operaEleH, width: operaEleW } =
-      operaEle.getBoundingClientRect();
-    const {
-      x: chatSpaceX,
-      y: chatSpaceY,
-      width: chatSpaceW
-    } = chatSpaceRef.current.getBoundingClientRect();
-    const {
-      x: msgSpaceX,
-      y: msgSpaceY,
-      width: msgSpaceW
-    } = e.target.getBoundingClientRect();
-    if (item.username === userInfo.username) {
-      if (msgSpaceX - chatSpaceX - operaEleW - 24 > operaEleW) {
-        operaEle.style.top = nextSbling ? '35%' : '45%';
-        operaEle.style.left = msgSpaceX - chatSpaceX - operaEleW - 24 + 'px';
-      } else {
-        operaEle.style.left = msgSpaceX - chatSpaceX - 20 + 'px';
-        if (msgSpaceY - chatSpaceY < operaEleH + 2) {
-          operaEle.style.top = 'auto';
-          operaEle.style.bottom = (nextSbling ? -4 : -operaEleH - 4) + 'px';
-        } else {
-          operaEle.style.top = 0 + 'px';
-        }
-      }
-    } else {
-      if (
-        chatSpaceW - (msgSpaceX - chatSpaceX) - operaEleW - msgSpaceW - 24 >
-        operaEleW
-      ) {
-        operaEle.style.top = nextSbling ? '35%' : '45%';
-        operaEle.style.right =
-          chatSpaceW -
-          (msgSpaceX - chatSpaceX) -
-          operaEleW -
-          msgSpaceW -
-          28 +
-          'px';
-      } else {
-        operaEle.style.right =
-          chatSpaceW - (msgSpaceX - chatSpaceX) - msgSpaceW - 24 + 'px';
-        if (msgSpaceY - chatSpaceY < operaEleH + 4) {
-          operaEle.style.top = 'auto';
-          operaEle.style.bottom = (nextSbling ? -4 : -operaEleH - 4) + 'px';
-        } else {
-          operaEle.style.top = '0';
-        }
-      }
-    }
+    operaEle.style.display = 'flex';
     requestAnimationFrame(() => {
-      operaEle.style.transition = 'all 0.2s ease-in-out';
-      operaEle.classList.add('opacity100');
+      const nextSbling = e.target.nextElementSibling;
+      const { height: operaEleH, width: operaEleW } =
+        operaEle.getBoundingClientRect();
+      const {
+        x: chatSpaceX,
+        y: chatSpaceY,
+        width: chatSpaceW
+      } = chatSpaceRef.current.getBoundingClientRect();
+      const {
+        x: msgSpaceX,
+        y: msgSpaceY,
+        width: msgSpaceW
+      } = e.target.getBoundingClientRect();
+      if (item.username === userInfo.username) {
+        if (msgSpaceX - chatSpaceX - operaEleW - 24 > operaEleW) {
+          operaEle.style.top = nextSbling ? '35%' : '45%';
+          operaEle.style.left = msgSpaceX - chatSpaceX - operaEleW - 24 + 'px';
+        } else {
+          operaEle.style.left = msgSpaceX - chatSpaceX - 20 + 'px';
+          if (msgSpaceY - chatSpaceY < operaEleH + 2) {
+            operaEle.style.top = 'auto';
+            operaEle.style.bottom = (nextSbling ? -4 : -operaEleH - 4) + 'px';
+          } else {
+            operaEle.style.top = 0 + 'px';
+          }
+        }
+      } else {
+        if (
+          chatSpaceW - (msgSpaceX - chatSpaceX) - operaEleW - msgSpaceW - 24 >
+          operaEleW
+        ) {
+          operaEle.style.top = nextSbling ? '35%' : '45%';
+          operaEle.style.right =
+            chatSpaceW -
+            (msgSpaceX - chatSpaceX) -
+            operaEleW -
+            msgSpaceW -
+            28 +
+            'px';
+        } else {
+          operaEle.style.right =
+            chatSpaceW - (msgSpaceX - chatSpaceX) - msgSpaceW - 24 + 'px';
+          if (msgSpaceY - chatSpaceY < operaEleH + 4) {
+            operaEle.style.top = 'auto';
+            operaEle.style.bottom = (nextSbling ? -4 : -operaEleH - 4) + 'px';
+          } else {
+            operaEle.style.top = '0';
+          }
+        }
+      }
+      requestAnimationFrame(() => {
+        operaEle.style.transition = 'all 0.2s ease-in-out';
+        operaEle.classList.add('opacity100');
+      });
     });
   };
   const userOperaControlForLeave = (e: any) => {
@@ -103,8 +106,11 @@ const ChatSpace = (props: any) => {
       e.target.parentElement.parentElement.parentElement.querySelector(
         '#opera'
       );
-    operaEle.style.transition = 'none';
     operaEle.classList.remove('opacity100');
+    setTimeout(() => {
+      operaEle.style.transition = 'none';
+      operaEle.style.display = '';
+    }, 200);
   };
 
   //点赞逻辑
@@ -273,7 +279,7 @@ const ChatSpace = (props: any) => {
             <div className="tw-flex tw-gap-2 tw-relative">
               <div
                 id="opera"
-                className={`tw-flex tw-z-50 tw-px-1 tw-py-0.5 tw-gap-1 opacity0 hover:tw-opacity-100 tw-text-xs tw-absolute opacity0 tw-h-fit tw-w-fit tw-rounded-md tw-bg-messageBackground`}
+                className={`tw-hidden tw-z-50 tw-px-1 tw-py-0.5 tw-gap-1 opacity0 hover:tw-opacity-100 hover:tw-flex tw-text-xs tw-absolute opacity0 tw-h-fit tw-w-fit tw-rounded-md tw-bg-messageBackground`}
               >
                 <div className="tw-rounded tw-cursor-pointer hover:tw-text-hoverColor hover:tw-bg-midGray tw-p-0.5 tw-flex tw-justify-center tw-items-center">
                   <RollbackOutlined />
